779db20471b9d51876ab953e04b921a2bd8040da
Johnny <104537366+johnnysedh3lllo@users.noreply.github.com>
2024-01-30 20:06:51 +0100

    minor adjustments

---
+++ public/script.js
@@ -0,0 +1,24 @@
+// "use strict";
+
+const cardBody = document.querySelector(".card__body");
+const cardAnswer = document.querySelector(".card__question__ans");
+const cardQuestion = document.querySelector(".card__question");
+
+cardBody.addEventListener("click", function (e) {
+  const clickedQuestion = e.target.closest(".card__question");
+
+  if (clickedQuestion) {
+    clickedQuestion
+      .querySelector(".card__question__ans")
+      .classList.toggle("closed");
+
+    const img = clickedQuestion.querySelector("img");
+    img.src = clickedQuestion
+      .querySelector(".card__question__ans")
+      .classList.contains("closed")
+      ? "images/icon-plus.svg"
+      : "images/icon-minus.svg";
+
+    console.log(img);
+  }
+});
