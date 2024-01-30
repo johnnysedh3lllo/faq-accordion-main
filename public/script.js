f29c5107dd0e03d0f81b7639bd01c6fa5bf6a7ba
Johnny <104537366+johnnysedh3lllo@users.noreply.github.com>
2024-01-31 00:00:48 +0100

    added final auto close feature

--- public/script.js
+++ public/script.js
@@ -1,24 +1,36 @@
 // "use strict";
 
+const card = document.querySelector(".card");
 const cardBody = document.querySelector(".card__body");
-const cardAnswer = document.querySelector(".card__question__ans");
-const cardQuestion = document.querySelector(".card__question");
+const cardAnswers = document.querySelectorAll(".card__question__ans");
+const cardQuestions = document.querySelectorAll(".card__question");
 
 cardBody.addEventListener("click", function (e) {
   const clickedQuestion = e.target.closest(".card__question");
 
-  if (clickedQuestion) {
-    clickedQuestion
-      .querySelector(".card__question__ans")
-      .classList.toggle("closed");
+  if (!clickedQuestion) return;
 
-    const img = clickedQuestion.querySelector("img");
-    img.src = clickedQuestion
-      .querySelector(".card__question__ans")
-      .classList.contains("closed")
-      ? "images/icon-plus.svg"
-      : "images/icon-minus.svg";
+  const answer = clickedQuestion.querySelector(".card__question__ans");
+  const img = clickedQuestion.querySelector("img");
 
-    console.log(img);
+  cardQuestions.forEach((question) => {
+    if (question !== clickedQuestion) {
+      const otherQuestion = question.querySelector(".card__question__ans");
+      const otherQuestionImg = question.querySelector("img");
+
+      otherQuestion.classList.remove("open");
+      otherQuestion.classList.add("closed");
+      otherQuestionImg.src = "images/icon-plus.svg";
+    }
+  });
+
+  if (answer.classList.contains("closed")) {
+    answer.classList.toggle("closed");
+    answer.classList.toggle("open");
+    img.src = "images/icon-minus.svg";
+  } else {
+    answer.classList.toggle("open");
+    answer.classList.add("closed");
+    img.src = "images/icon-plus.svg";
   }
 });
