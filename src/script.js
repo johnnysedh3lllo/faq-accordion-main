a35f57c196096a7782f4e735c506ade5e578d35a
Johnny <104537366+johnnysedh3lllo@users.noreply.github.com>
2024-02-02 13:44:44 +0100

    fixed javascript icon loading with import statements

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
 // "use strict";
-const card = document.querySelector(".card");
+import plusIcon from "./assets/icon-plus.svg";
+import minusIcon from "./assets/icon-minus.svg";
+
 const cardBody = document.querySelector(".card__body");
 const cardQuestions = document.querySelectorAll(".card__question");
 
 const closeQuestion = function (question) {
   question.querySelector(".card__question__ans").classList.remove("open");
   question.querySelector(".card__question__ans").classList.add("closed");
-  question.querySelector("img").src = "src/assets/icon-plus.svg";
+  question.querySelector("img").src = plusIcon;
 };
 
 cardBody.addEventListener("click", function (e) {
@@ -26,11 +28,11 @@ cardBody.addEventListener("click", function (e) {
   if (answer.classList.contains("closed")) {
     answer.classList.toggle("closed");
     answer.classList.toggle("open");
-    img.src = "src/assets/icon-minus.svg";
+    img.src = minusIcon;
   } else {
     answer.classList.toggle("open");
     answer.classList.toggle("closed");
-    img.src = "src/assets/icon-plus.svg";
+    img.src = plusIcon;
   }
 });
 
